e7611907159f60f76b2380429abe33d4ff7274aa
Vincent <vsuen123@gmail.com>
2023-06-28 20:50:01 -0400

    test

--- client/components/form/RegisterForm.jsx
+++ client/components/form/RegisterForm.jsx
@@ -1,52 +1,59 @@
-"use client"
+"use client";
 import { useState } from "react";
 
 const RegisterForm = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [success, setSuccess] = useState(null);
+  const [error, setError] = useState(null);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const res = await fetch(`https://dialogue-api-v1nce-dev.vercel.app/api/users/register/`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ username: username, password: password }),
-            });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch(`http://localhost:8080/api/users/register`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username: username, password: password }),
+      });
 
-            if (res.ok) {
-                console.log("Registration Successful");
-                setUsername("");
-                setPassword("");
-            }
-        } catch (err) {
-            console.error("Error:", err);
-        }
-    };    
+      if (!res.ok) {
+        const data = await res.json();
+        throw new Error(data.message);
+      }
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input 
-                    type="text" 
-                    placeholder="Username" 
-                    className="border border-black"
-                    value={username} 
-                    onChange={(e) => setUsername(e.target.value)} 
-                />
-                <input 
-                    type="password" 
-                    placeholder="Password" 
-                    className="border border-black"
-                    value={password} 
-                    onChange={(e) => setPassword(e.target.value)} 
-                />
-                <button type="submit">Register</button>
-            </form>
-        </div>
-    )
-}
+      setSuccess("Registration Successful");
+      setUsername("");
+      setPassword("");
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          className="border border-black"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="border border-black"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Register</button>
+      </form>
+      {error && <p>{error}</p>}
+      {success && <p>{success}</p>}
+    </div>
+  );
+};
 
 export default RegisterForm;
